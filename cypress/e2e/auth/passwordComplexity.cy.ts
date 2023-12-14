c0b9e2d749660be04fa49c20cfc51af9523a4a8a
ushka1 <david.prajzner01@gmail.com>
2023-12-14 20:19:24 +0100

    add optional tests flag

--- cypress/e2e/auth/passwordComplexity.cy.ts
+++ cypress/e2e/auth/passwordComplexity.cy.ts
@@ -26,8 +26,9 @@ afterEach(() => {
 });
 
 describe('Password complexity', () => {
-  const baseUrl = Cypress.config('baseUrl');
   let password: string;
+  const baseUrl = Cypress.config('baseUrl');
+  const runOptionalTests = Cypress.env('RUN_OPTIONAL_TESTS');
 
   it('Displays error for empty password', () => {
     cy.visit('/account/register');
@@ -63,25 +64,28 @@ describe('Password complexity', () => {
       .should('not.be.empty');
   });
 
-  it('Displays error for incomplete password (missing character groups)', () => {
-    password = 'abcabcabcabcabc';
-    cy.intercept('POST', '/api/customers/sessions').as('login');
+  // OPTIONAL
+  if (runOptionalTests) {
+    it('Displays error for incomplete password (missing character groups)', () => {
+      password = 'abcabcabcabcabc';
+      cy.intercept('POST', '/api/customers/sessions').as('login');
 
-    cy.visit('/account/register');
-    cy.get('.register-form input[name="full_name"]').type(
-      customerData.fullName,
-    );
-    cy.get('.register-form input[name="email"]').type(customerData.email);
-    cy.get('.register-form input[name="password"]').type(password);
-    cy.get('.register-form button[type="button"]').click();
+      cy.visit('/account/register');
+      cy.get('.register-form input[name="full_name"]').type(
+        customerData.fullName,
+      );
+      cy.get('.register-form input[name="email"]').type(customerData.email);
+      cy.get('.register-form input[name="password"]').type(password);
+      cy.get('.register-form button[type="button"]').click();
 
-    cy.wait('@login');
+      cy.wait('@login');
 
-    cy.url().should('eq', `${baseUrl}/account/register`);
-    cy.get('.register-form .text-critical')
-      .invoke('text')
-      .should('not.be.empty');
-  });
+      cy.url().should('eq', `${baseUrl}/account/register`);
+      cy.get('.register-form .text-critical')
+        .invoke('text')
+        .should('not.be.empty');
+    });
+  }
 
   it('Successfully creates account or changes password with valid password', () => {
     password = 'sk[MV&v-898h[lllprv[DUo';
@@ -101,7 +105,8 @@ describe('Password complexity', () => {
     cy.get('.account-details-email').should('contain', customerData.email);
   });
 
-  // it('Enforces custom password policy for admin (Optional)', () => {
-  //   //
-  // });
+  // OPTIONAL
+  if (runOptionalTests) {
+    it('Enforces custom password policy for admin (Optional)', () => {});
+  }
 });
