d5b318990a7b149291502bd135ab78c8abd6659c
“Krzysztof <krzysztof.gebka2001@gmail.com>
2024-01-10 11:43:45 +0100

    Refactor displayCategories.cy.ts to handle empty list of categories

--- cypress/e2e/catalog/categories/displayCategories.cy.ts
+++ cypress/e2e/catalog/categories/displayCategories.cy.ts
@@ -3,15 +3,9 @@ describe('Display categories', () => {
     const adminEmail = Cypress.env('ADMIN_EMAIL')
     const adminPassword = Cypress.env('ADMIN_PASSWORD')
 
-    it('go to categories', () => {
-        cy.exec('docker exec shopmost-db pg_restore -h localhost -U postgres -d shopmost -F c -c -v plik_dumpu.pgdump')
-        cy.visit('/admin/login')
-        cy.location('pathname').should('eq', '/admin/login')
-        cy.get("#adminLoginForm").get('input[name="email"]').type(adminEmail)
-        cy.get("#adminLoginForm").get('input[name="password"]').type(adminPassword)
-        cy.get('.button').click()
-        cy.location('pathname').should('eq', '/admin')
-        cy.get('.list-unstyled').contains('Categories').click()
-        cy.location('pathname').should('eq', '/admin/categories')
+    it('empty list of categories', () => {
+        cy.loadDatabaseDump('empty_categories_dump')
+        cy.goToCategories(adminEmail, adminPassword)
+        cy.get('.card').should('contain', 'There is no category to display')
     });
 });
